d5f590bbce4d37250606696316e046bed3bea8e6
akki-mali <akshitamali1995@gmail.com>
2025-06-19 19:32:14 +0530

    igniting the app with parcel

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-const heading = React.createElement("h1", {id: 'header'}, "Hello World from React!");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("div", {id: 'parent'}, [
+    React.createElement("h1", {id: 'header'}, "Hello From React"),
+    React.createElement("p", {id: 'description'}, "This is a paragraph")
+]) ;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
